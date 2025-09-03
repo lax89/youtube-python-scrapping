28156799eed67ddf30f2cd16f337074547fb74f3
unknown <laxminarayanarath6@gmail.com>
2025-09-03 14:47:41 +0530

    updated

--- render-build.sh
+++ render-build.sh
@@ -2,7 +2,10 @@
 # apt-get update
 # xargs -a apt.txt apt-get install -y
 <<<<<<< HEAD
+<<<<<<< HEAD
 pip install -r requirements.txt
+=======
+>>>>>>> 780e7b49a5d68496c494220c451354474ff20d6e
 =======
 >>>>>>> 780e7b49a5d68496c494220c451354474ff20d6e
 playwright install chromium
