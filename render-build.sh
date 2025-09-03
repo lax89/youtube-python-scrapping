bc151f04ebf284315e342319dc3869e596b9d106
unknown <laxminarayanarath6@gmail.com>
2025-09-03 12:14:31 +0530

    updated render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-apt-get update
-xargs -a apt.txt apt-get install -y
+# apt-get update
+# xargs -a apt.txt apt-get install -y
+pip install -r requirements.txt
+playwright install chromium
